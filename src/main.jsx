8deb9b553d39725783a650a0a35a6db9ae9b18ec
Karan Singh Rawat <singhrawatk2456@gmail.com>
2026-08-09 11:32:56 +0530

    Simplify import statements in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import "./index.css";
 
 import App from "./App.jsx";
 
-import {
-  Home,
-  Login,
-  SignUp,
-  Dashboard,
-} from "./components/Import";
+import { Home, Login, SignUp, Dashboard } from "./components/Import";
 
 const router = createBrowserRouter([
   {
