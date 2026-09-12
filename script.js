d7428d6a408e19e3875ddf110e75fa02fcccfe04
Karan Singh Rawat <singhrawatk2456@gmail.com>
2026-09-12 12:46:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-let P = 1000
-let r = 0.02
-let n = 1
-let t = 2
-let A = P * ((1 + (r / n)) ** (n * t))
-let CI = A - P
 
-console.log("The Amount after the Compound Intrest is :", A);
-console.log("Compound intrest after", t, "years is :", CI);
